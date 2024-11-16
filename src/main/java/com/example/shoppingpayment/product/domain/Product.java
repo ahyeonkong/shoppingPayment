5aef5c2451de20d64f6d869bf4e9d0ce3d3fa80c
ahyeonkong <kimah0403@gmail.com>
2024-11-16 17:16:32 +0900

    refactor: Product 엔티티 구조 수정
    - null 값 여부(nullable) 명시
    - 필드 길이(length) 제한 추가
    - @CreationTimestamp, @UpdateTimestamp 어노테이션 적용
    - private 접근 제어자 명시
    
    Resolves: #15

--- src/main/java/com/example/shoppingpayment/product/domain/Product.java
+++ src/main/java/com/example/shoppingpayment/product/domain/Product.java
@@ -5,32 +5,45 @@ import com.example.shoppingpayment.orderItem.domain.OrderItem;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Setter
 @Getter
 @Entity
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long product_id;
-    @Column
-    String product_name;
-    @Column
-    Long product_price;
-    @Column
-    String product_description;
-    @Column
-    String product_image_url;
-    @Column
-    String product_category;
-    @Column
-    LocalDate product_created_at;
-    @Column
-    LocalDate product_updated_at;
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "product_name", nullable = false, length = 100)
+    private String productName;
+
+    @Column(name = "product_price", nullable = false)
+    private Long productPrice;
+
+    @Column(name = "product_description", length = 50)
+    private String productDescription;
+
+    @Column(name = "product_image_url", length = 255)
+    private String productImageUrl;
+
+    @Column(name = "product_category", nullable = false, length = 50)
+    private String productCategory;
+
+    @CreationTimestamp
+    @Column(name = "product_created_at", nullable = false, updatable = false)
+    private LocalDateTime productCreatedAt;
+
+    @UpdateTimestamp
+    @Column(name = "product_updated_at")
+    private LocalDateTime productUpdatedAt;
 
     @OneToMany(mappedBy = "product")
     private List<CartItem> cartItems = new ArrayList<>();
